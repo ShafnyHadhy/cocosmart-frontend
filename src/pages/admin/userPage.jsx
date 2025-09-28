1a70afd635ebb3f270e522fb3ccb1b936c9e952d
David_Billa_AJ <agashbilla2001@gmail.com>
2025-09-28 14:28:10 +0530

    Design User Page Role..

--- src/pages/admin/userPage.jsx
+++ src/pages/admin/userPage.jsx
@@ -176,16 +176,16 @@ const UserPage = () => {
                     </select>
                   ) : (
                     <span
-                      className={`px-2 py-1 rounded text-xs font-medium ${
+                      className={`px-3 py-1 rounded text-xs font-medium ${
                         user.role === "admin"
-                          ? "bg-red-100 text-red-800"
+                          ? "bg-red-100 text-red-800 px-5 py-1 rounded-full font-semibold"
                           : user.role === "worker"
-                          ? "bg-yellow-100 text-yellow-800"
+                          ? "bg-yellow-100 text-yellow-800 px-4 py-1 rounded-full font-semibold"
                           : user.role === "Pro.Staff"
-                          ? "bg-blue-100 text-blue-800"
+                          ? "bg-blue-100 text-blue-800 rounded-full font-semibold"
                           : user.role === "HRM"
-                          ? "bg-green-100 text-green-800"
-                          : "bg-gray-100 text-gray-800"
+                          ? "bg-green-200 text-green-800 px-7 py-1 rounded-full font-semibold"
+                          : "bg-gray-100 text-gray-800 px-7 py-1 rounded-full font-semibold"
                       }`}
                     >
                       {formatRoleName(user.role)}
